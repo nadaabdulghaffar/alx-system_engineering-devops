97f6240313792cc2a7ddf75fa36036d5ccbd7c05
= <nadaabdulghaffar42@gmail.com>
2024-07-03 13:26:45 +0300

    Task 1: puppt-lint fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a File in /tmp/school
 file { '/tmp/school':
   ensure  => 'file',
-  content => "I love Puppet",
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
