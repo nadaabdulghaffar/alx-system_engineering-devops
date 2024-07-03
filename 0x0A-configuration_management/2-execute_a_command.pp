27961a385e45cf27554e4a97f63dfedb2b366f31
= <nadaabdulghaffar42@gmail.com>
2024-07-03 14:25:14 +0300

    task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named killmenow
-exec { 'pkill killmenow':
-  command  => 'pkill killmenow',
+exec { 'kill_killmenow':
+  command  => 'kill_killmenow',
   provider => shell,
   path     => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
 }
