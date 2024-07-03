8371277fcaa064e1ae0d02e2817a078f338f0ce5
= <nadaabdulghaffar42@gmail.com>
2024-07-03 14:31:55 +0300

    task 3: Rename resourseTitle

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow
-exec { 'kill_killmenow':
+exec { 'pkill killmenow':
   command  => 'kill_killmenow',
   provider => shell,
   path     => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
